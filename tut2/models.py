4e8dfc8d0dd740765912d96e1fccba3b180fa088
Ridwan <aladebaruwa@gmail.com>
2024-06-10 05:33:56 +0100

    add

--- tut2/models.py
+++ tut2/models.py
@@ -3,13 +3,4 @@ from pydantic import BaseModel
 class User(BaseModel):
     name: str
     age: int
-    users = {
-    "1": {
-        "name": "John",
-        "age": 20
-    },
-    "2": {
-        "name": "Jane",
-        "age": 21
-    }
-}
+   
