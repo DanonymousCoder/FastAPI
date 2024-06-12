cb3c2ceee7afc7a18ded65686632235ad8d3b3ba
Ridwan <aladebaruwa@gmail.com>
2024-06-12 05:25:10 +0100

    add

--- tut3/app/controllers.py
+++ tut3/app/controllers.py
@@ -4,3 +4,6 @@ from . import models, schemas
 
 def get_items(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Item).offset(skip).limit(limit).all()
+
+def get_item(db: Session, item_id: int):
+    return db.query(models.Item).filter(models.Item.id == item_id).first()
